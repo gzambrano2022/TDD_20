ff4ede29c73c14af07178c6e4b3139b12ce8add2
Gabriela Zambrano <gzambrano2022@udec.cl>
2025-08-20 16:23:18 -0400

    RED: test para generar valor en dado - falla como esperado

--- tests/test_dado.py
+++ tests/test_dado.py
@@ -1,8 +1,13 @@
 import unittest
+from src.juego.dado import dado
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class TestGenerarValor(unittest.TestCase):
+    def test_generar_valor(self):
+        # generar valor del dado en rangos normales (1 a 6)
+        valor = dado()
+        self.assertGreaterEqual(valor, 1)
+        self.assertLessEqual(valor, 6)
 
 if __name__ == '__main__':
     unittest.main()
