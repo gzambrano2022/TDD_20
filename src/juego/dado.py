9e2c2279404966aee2ec48e44d3a7849518a15a4
Gabriela Zambrano <gzambrano2022@udec.cl>
2025-08-20 17:08:49 -0400

    GREEN: implementación mínima para generar valor en dado

---
+++ src/juego/dado.py
@@ -0,0 +1,8 @@
+import random
+
+class Dado:
+    def generar_valor(self):
+        return random.randint(1,6)
+
+    # def __init__(self, nombre):
+     #   self.nombre = nombre
--- tests/test_dado.py
+++ tests/test_dado.py
@@ -1,15 +1,14 @@
 import unittest
-"""
-from src.juego.dado import dado
+from src.juego.dado import Dado
 
 
 class TestGenerarValor(unittest.TestCase):
     def test_generar_valor(self):
+        dado = Dado()
         # generar valor del dado en rangos normales (1 a 6)
-        valor = dado()
+        valor = dado.generar_valor()
         self.assertGreaterEqual(valor, 1)
         self.assertLessEqual(valor, 6)
 
 if __name__ == '__main__':
     unittest.main()
-"""
