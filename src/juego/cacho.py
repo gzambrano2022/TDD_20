fcf6be8cfd1449478b3b4e8528b5817d84cbbf19
Gabriela Zambrano <gzambrano2022@udec.cl>
2025-08-22 18:37:22 -0400

    GREEN: implementación mínima para agitar/regenerar valores en los dados

--- src/juego/cacho.py
+++ src/juego/cacho.py
@@ -9,3 +9,8 @@ class Cacho:
         for i in range(5):
             self.almacen.append(self.dado.generar_valor())
         return self.almacen
+
+    def agitar_dados(self):
+        for i in range(len(self.almacen)):
+            self.almacen[i] = self.dado.generar_valor()
+        return self.almacen
