e839c82a83b0036ee29f685ce82741b7622e52ad
d-a-t-s <dtamaro2023@udec.cl>
2025-08-23 20:57:58 -0400

    GREEN: implementacion minima para contador_pintas

---
+++ src/juego/contador_pintas.py
@@ -0,0 +1,5 @@
+class ContadorPintas:
+
+    #Metodo para contar pintas dada una lista de dados (enteros) y una pinta (entero)
+    def contar_pintas(self,dados,pinta):
+        return dados.count(pinta)
--- tests/test_contador_pintas.py
+++ tests/test_contador_pintas.py
@@ -4,5 +4,5 @@ def test_contar_pintas():
     dados = [1,2,5,4,3,3,3,5,4,5] #Ejemplo con 10 dados
     pinta = 3 #Pinta que queremos contar
     contador = ContadorPintas() #Instancia del objeto ContadorPintas
-    resultado = contador.contar_pintass(dados,pinta) #Llamada al metodo para contar pintas
+    resultado = contador.contar_pintas(dados,pinta) #Llamada al metodo para contar pintas
     assert resultado == 3 #Verificacion del resultado esperado
