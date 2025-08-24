ad86e4050f01657e8b735eacb7347b443b157576
d-a-t-s <dtamaro2023@udec.cl>
2025-08-23 21:33:14 -0400

    RED: test para contar pintas en una ronda especial donde los ases no cuentan como comodines

--- tests/test_contador_pintas.py
+++ tests/test_contador_pintas.py
@@ -7,3 +7,12 @@ def test_contar_pintas():
     contador = ContadorPintas() #Instancia del objeto ContadorPintas
     resultado = contador.contar_pintas(dados,pinta) #Llamada al metodo para contar pintas
     assert resultado == 9 #Verificacion del resultado esperado
+
+#Cuenta las pintas en una ronda especial cuando un jugador se queda con un solo dado por primera vez
+#los ases cuentan como uno solamente y no como comodines
+def test_contar_pintas_en_ronda_de_un_solo_dado():
+    dados = [1,2,3,4,5,3,2,1,4,3,4] #Ejemplo con 11 dados, dos jugadores con 5 dados y uno con 1 dado
+    pinta = 4 #Pinta que queremos contar
+    contador = ContadorPintas() #Instancia del objeto ContadorPintas
+    resultado = contador.contar_pintas(dados,pinta) #Llamada al metodo para contar pintas
+    assert resultado == 3 #Verificacion del resultado esperado
