dc17a3949e0ee1178d2e76d6f34ab24b1f685a11
Luis <lumartinez2023@udec.cl>
2025-08-24 16:08:02 -0400

    GREEN: implementación mínima para elegir jugador inicial

--- src/juego/gestor_partida.py
+++ src/juego/gestor_partida.py
@@ -1,4 +1,5 @@
 from src.juego.persona import Persona
+import random
 class Gestor_partida:
     def __init__(self):
         self.jugadores = []
@@ -6,3 +7,10 @@ class Gestor_partida:
     def crear_jugadores(self,cantidad):
         for i in range(cantidad):
             self.jugadores.append(Persona(i))
+
+    def jugador_inicial(self,lista):
+        mayor = 0
+        for i in range(len(lista)):
+            if lista[i] > mayor:
+                mayor = lista[i]
+        return mayor
