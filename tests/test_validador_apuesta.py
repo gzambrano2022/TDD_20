481cf478ba5ffa9ec5cad36f1dacd24ea16da8b7
d-a-t-s <dtamaro2023@udec.cl>
2025-08-23 22:53:53 -0400

    Fix: La funcionalidad para validar apuestas solo considera la apuesta valida si el numero de apariciones o la pinta aumentan, es decir, no se pueden aumentar una pero disminuir la otra

--- tests/test_validador_apuesta.py
+++ tests/test_validador_apuesta.py
@@ -3,12 +3,14 @@ from src.juego.validador_apuesta import ValidadorApuesta
 #Una apuesta valida será aquella en la que aumenta la pinta o la cantidad de aparaciones de la pinta
 def test_apuesta_valida():
     validador = ValidadorApuesta() #Instancia el objeto ValidadorApuesta
-    
+
     #Definiremos una apuesta como un par ordenado (cantidad_de_apariciones, pinta)
     apuesta_inicial = (3,4) #tres cuartas
 
     apuesta_nueva1 = (3,5) #tres quintas
     apuesta_nueva2 = (4,4) #cuatro cuartas
+    apuesta_nueva3 = (4,5) #cuatro quintas (caso en el que ambos aumentan tambien es valido)
 
     assert validador.apuesta_valida(apuesta_inicial, apuesta_nueva1) == True
     assert validador.apuesta_valida(apuesta_inicial, apuesta_nueva2) == True
+    assert validador.apuesta_valida(apuesta_inicial, apuesta_nueva3) == True
