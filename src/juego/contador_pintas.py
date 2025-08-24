b5dcf1d0a932a11cbf00f6c547ca90b53ff1d49c
d-a-t-s <dtamaro2023@udec.cl>
2025-08-23 21:23:03 -0400

    GREEN: implementacion minima para contar pintas considerando los ases como comodines

--- src/juego/contador_pintas.py
+++ src/juego/contador_pintas.py
@@ -2,4 +2,8 @@ class ContadorPintas:
 
     #Metodo para contar pintas dada una lista de dados (enteros) y una pinta (entero)
     def contar_pintas(self,dados,pinta):
-        return dados.count(pinta)
+        cont = 0
+        for dado in dados:
+            if dado == 1 or dado == pinta:
+                cont += 1
+        return cont
