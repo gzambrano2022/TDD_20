22efd4cfab240f19cb46f59afa74e8813012ecec
d-a-t-s <dtamaro2023@udec.cl>
2025-08-23 21:18:14 -0400

    RED: test para contar pintas considerando ases como comodines - falla como esperado

--- tests/test_contador_pintas.py
+++ tests/test_contador_pintas.py
@@ -1,8 +1,9 @@
 from src.juego.contador_pintas import ContadorPintas
 
+#Cuenta las pintas en una lista de dados tratando los ases como comodines
 def test_contar_pintas():
-    dados = [1,2,5,4,3,3,3,5,4,5] #Ejemplo con 10 dados
+    dados = [1,2,5,4,3,3,3,5,4,5,1,1,1,1,1] #Ejemplo con 15 dados
     pinta = 3 #Pinta que queremos contar
     contador = ContadorPintas() #Instancia del objeto ContadorPintas
     resultado = contador.contar_pintas(dados,pinta) #Llamada al metodo para contar pintas
-    assert resultado == 3 #Verificacion del resultado esperado
+    assert resultado == 9 #Verificacion del resultado esperado
