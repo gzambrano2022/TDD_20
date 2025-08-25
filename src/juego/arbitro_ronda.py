c07610e622a8040eec8f5a54b1dad1e34c9c8e44
Luis <lumartinez2023@udec.cl>
2025-08-25 05:48:57 -0400

    REFACTOR: mejora código de funcion dudar

--- src/juego/arbitro_ronda.py
+++ src/juego/arbitro_ronda.py
@@ -1,14 +1,20 @@
 class Arbitro_ronda:
 
-    def dudar(self,apuesta,jugadores):
+    def dudar(self,apuesta,jugadores,jugador_actual):
         cantidad_jugadores = len(jugadores)
         numero = apuesta[1]
         cantidad_dados = 0
         for i in range(cantidad_jugadores):
             cantidad_dados = cantidad_dados + jugadores[i].cacho.almacen.count(numero)
         if cantidad_dados >= apuesta[0]:
+            jugadores[jugador_actual - 1].cacho.perder_dado()
             return "pierde"
         else:
+            if jugador_actual - 1 == 0:
+                jugador_anterior = cantidad_jugadores - 1
+                jugadores[jugador_anterior].cacho.perder_dado()
+            else:
+                jugadores[jugador_actual - 2].cacho.perder_dado()
             return "gana"
 
 
--- tests/test_arbitro_ronda.py
+++ tests/test_arbitro_ronda.py
@@ -4,9 +4,14 @@ from src.juego.gestor_partida import Gestor_partida
 def test_dudar():
     gestor = Gestor_partida()
     gestor.crear_jugadores(2)
+    gestor.jugador_actual = 1
     gestor.jugadores[0].cacho.almacen = [1,2,2,3,3]
-    gestor.jugadores[1].cacho.almacen = [5,5,3,1,4]
+    gestor.jugadores[1].cacho.almacen = [5,2,2,1,4]
     apuesta = (3,3)
     arbitro = Arbitro_ronda()
-    assert arbitro.dudar(apuesta,gestor.jugadores) == "pierde"
-
+    assert arbitro.dudar(apuesta,gestor.jugadores,gestor.jugador_actual) == "gana"
+    assert len(gestor.jugadores[1].cacho.almacen) == 4
+    gestor.jugadores[0].cacho.almacen = [1,2,2,3,3]
+    gestor.jugadores[1].cacho.almacen = [5,3,3,1,4]
+    assert arbitro.dudar(apuesta,gestor.jugadores,gestor.jugador_actual) == "pierde"
+    assert len(gestor.jugadores[0].cacho.almacen) == 4
