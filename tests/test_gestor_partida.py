7ecc09ca0def0e0e49ff251c9fcf1a411162d24a
Luis <lumartinez2023@udec.cl>
2025-08-25 04:38:59 -0400

    RED: test para detectar si alguien queda con un dado - falla como esperado

--- tests/test_gestor_partida.py
+++ tests/test_gestor_partida.py
@@ -7,14 +7,14 @@ class Test_gestor:
         cantidad = 5
         gestor.crear_jugadores(cantidad)
     
-    @patch("random.randint", side_effect=[2, 2, 1,1])
+    @patch("src.juego.gestor_partida.random.randint", side_effect=[2, 2, 1,1])
     def test_jugador_inicial(self,mock_randint):
         gestor= Gestor_partida()
         gestor.crear_jugadores(3)
         gestor.jugador_inicial()
         assert gestor.jugador_actual == 2
 
-    @patch("random.randint", side_effect=[1,2,3])
+    @patch("src.juego.gestor_partida.random.randint", side_effect=[1,2,3])
     def test_pasarturno(self,mock_randint):
         gestor= Gestor_partida()
         gestor.crear_jugadores(3)
@@ -22,9 +22,15 @@ class Test_gestor:
         assert gestor.jugador_actual == 3
         gestor.pasar_turno()
         assert gestor.jugador_actual == 1
-        
 
-
+    def test_verificar_un_dado(self):
+        gestor= Gestor_partida()
+        gestor.crear_jugadores(3)
+        for i in range(3):
+            gestor.jugadores[i].cacho.almacenar_dados()
+        assert gestor.verificar_un_dado() == 0
+        gestor.jugadores[2].cacho.almacen = [1]
+        assert gestor.verificar_un_dado() == 3
 
 
 """
