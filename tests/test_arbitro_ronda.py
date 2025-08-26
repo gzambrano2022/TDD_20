c0d383e41e20f5da4fb8af99973a2249553cbee0
Luis <lumartinez2023@udec.cl>
2025-08-25 22:00:56 -0400

    RED: test para funcionalidad de cacho, falla como esperado

--- tests/test_arbitro_ronda.py
+++ tests/test_arbitro_ronda.py
@@ -15,3 +15,18 @@ def test_dudar():
     gestor.jugadores[1].cacho.almacen = [5,3,3,1,4]
     assert arbitro.dudar(apuesta,gestor.jugadores,gestor.jugador_actual) == "pierde"
     assert len(gestor.jugadores[0].cacho.almacen) == 4
+
+def test_dudar():
+    gestor = Gestor_partida()
+    gestor.crear_jugadores(2)
+    gestor.jugador_actual = 1
+    gestor.jugadores[0].cacho.almacen = [1,2,4,4,3]
+    gestor.jugadores[1].cacho.almacen = [1,2,3,4,4]
+    apuesta = (3,4)
+    arbitro = Arbitro_ronda()
+    assert arbitro.cacho(apuesta,gestor.jugadores,gestor.jugador_actual) == "gana"
+    assert len(gestor.jugadores[gestor.jugador_actual - 1].cacho.count) == 1
+    gestor.jugadores[0].cacho.almacen = [1,2,2,3,3]
+    gestor.jugadores[1].cacho.almacen = [5,1,1,1,4]
+    assert arbitro.cacho(apuesta,gestor.jugadores,gestor.jugador_actual) == "pierde"
+    assert len(gestor.jugadores[gestor.jugador_actual - 1].cacho.almacen) == 4
