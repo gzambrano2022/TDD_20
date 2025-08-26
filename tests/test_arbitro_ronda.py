ebe76db2ef123a0f30f92c31e2b0f656d096ab9e
Luis <lumartinez2023@udec.cl>
2025-08-25 22:23:03 -0400

    RED: el test anterior debia ser para la funcionalidad de calzar, falla como esperado

--- tests/test_arbitro_ronda.py
+++ tests/test_arbitro_ronda.py
@@ -16,17 +16,18 @@ def test_dudar():
     assert arbitro.dudar(apuesta,gestor.jugadores,gestor.jugador_actual) == "pierde"
     assert len(gestor.jugadores[0].cacho.almacen) == 4
 
-def test_dudar():
+def test_calzar():
     gestor = Gestor_partida()
     gestor.crear_jugadores(2)
     gestor.jugador_actual = 1
-    gestor.jugadores[0].cacho.almacen = [1,2,4,4,3]
+    gestor.jugadores[0].cacho.almacen = [1,2,3,4,3]
     gestor.jugadores[1].cacho.almacen = [1,2,3,4,4]
     apuesta = (3,4)
     arbitro = Arbitro_ronda()
-    assert arbitro.cacho(apuesta,gestor.jugadores,gestor.jugador_actual) == "gana"
-    assert len(gestor.jugadores[gestor.jugador_actual - 1].cacho.count) == 1
+    assert arbitro.calzar(apuesta,gestor.jugadores,gestor.jugador_actual) == "gana"
+    assert gestor.jugadores[gestor.jugador_actual - 1].cacho.count == 1
     gestor.jugadores[0].cacho.almacen = [1,2,2,3,3]
     gestor.jugadores[1].cacho.almacen = [5,1,1,1,4]
-    assert arbitro.cacho(apuesta,gestor.jugadores,gestor.jugador_actual) == "pierde"
-    assert len(gestor.jugadores[gestor.jugador_actual - 1].cacho.almacen) == 4
+    assert arbitro.calzar(apuesta,gestor.jugadores,gestor.jugador_actual) == "pierde"
+    #Debera tener count igual a 1, por lo tanto no se le quitan dados
+    assert len(gestor.jugadores[gestor.jugador_actual - 1].cacho.almacen) == 5
